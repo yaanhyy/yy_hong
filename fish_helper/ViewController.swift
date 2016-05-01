48384b611bf059f9c43ad3d5899b2927460a7285
洪远洋 <hongyuanyang@hongyuanyangs-mac-mini.local>
2016-05-01 22:33:44 +0800

    Initial Commit

---
+++ fish_helper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fish_helper
+//
+//  Created by 洪远洋 on 5/1/16.
+//  Copyright © 2016 洪远洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
