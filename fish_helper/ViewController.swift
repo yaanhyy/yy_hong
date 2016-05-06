817be6d3e982f9d8dca254a0fb76d3f151862b21
洪远洋 <hongyuanyang@hongyuanyangs-mac-mini.local>
2016-05-06 17:48:28 +0800

    add  cacaAsync and implement ump send

--- fish_helper/ViewController.swift
+++ fish_helper/ViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import CocoaAsyncSocket
+class ViewController: UIViewController , GCDAsyncUdpSocketDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +20,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my first app!", preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Disai OK", style: .Default, handler: nil)
-        alert.addAction(action)
-        presentViewController(alert, animated: true, completion: nil)
+    //    let alert = UIAlertController(title: "Hello, World",
+     //                                 message: "This is my first app!", preferredStyle: .Alert)
+    //    let action = UIAlertAction(title: "Disai OK", style: .Default, handler: nil)
+     //   alert.addAction(action)
+     //   presentViewController(alert, animated: true, completion: nil)
+        //send login req
+        var address = "192.168.2.100"
+        var port:UInt16 = 23458
+        var socket:GCDAsyncUdpSocket!
+        var socketReceive:GCDAsyncUdpSocket!
+        var error : NSError?
+        let message = "love you!baby".dataUsingEncoding(NSUTF8StringEncoding)
+        socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: dispatch_get_main_queue())
+        socket.sendData(message, toHost: address, port: port, withTimeout: 1000, tag: 0)
+        do {
+        //    try socket.enableBroadcast(true)
+        } catch {
+            print(error)
+        }
+        
+    
     }
 }
 
