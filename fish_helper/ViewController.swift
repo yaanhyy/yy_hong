2767a755118c6800bfa965e3666687be9e4f1154
王少兰 <wsony@sohu.com>
2016-05-09 16:28:06 +0800

    add the devlistactivity ; add button onclick.

--- fish_helper/ViewController.swift
+++ fish_helper/ViewController.swift
@@ -8,23 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate{
 
+    //@IBOutlet weak var login_backgd: UIImageView!
+    @IBOutlet weak var user_name: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var result_message_lable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //login_backgd.backgroundColor
+        //btnLgoin.layer.cornerRadius=4.0
+        
+        
+        user_name.placeholder="请输入用户名"
+        user_name.becomeFirstResponder()
+        user_name.keyboardType = UIKeyboardType.NumberPad
+        user_name.returnKeyType = UIReturnKeyType.Next
+        password.returnKeyType = UIReturnKeyType.Done
+        password.placeholder="请输入密码"
+        password.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+   
+  
+    
+    func textFieldShouldReturn(textField:UITextField) -> Bool
+    {
+//        if textField == user_name {
+//            password.becomeFirstResponder()
+//        }
+//        else{
+             textField.resignFirstResponder()
+//        }
+  
+        return true;
+    }
+ 
+   
+    @IBAction func btn_login_onclick(sender: AnyObject) {        /**登陆验证成功**/
+        if user_name.text == "admin" && password.text == "123"{
+            /**进行跳转**/
+            self.performSegueWithIdentifier("btn_login_onclick", sender: nil)
+        }else{
+            result_message_lable.text = "username or passward is error!"
+        }
+    }
+    @IBAction func did_register_onclick(sender: UIButton) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my first app!", preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Disai OK", style: .Default, handler: nil)
-        alert.addAction(action)
-        presentViewController(alert, animated: true, completion: nil)
-    }
+    
+  
 }
 
